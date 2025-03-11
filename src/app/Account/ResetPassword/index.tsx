c6b553de8a3131d67dc015fba30f84a63e62c8f2
AbigailDeng <Abigail.deng@aelf.io>
2025-03-11 18:35:59 +0800

    feat: reset psw

--- src/app/Account/ResetPassword/index.tsx
+++ src/app/Account/ResetPassword/index.tsx
@@ -11,16 +11,66 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { useToast } from "@/hooks/use-toast";
+import { resetPassword } from "@/services/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-
+import { z } from "zod";
+const formSchema = z.object({
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+  confirmPassword: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
 const ResetPassword = () => {
-  const form = useForm();
-  function onSubmit(values: any) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+  const [userId, setUserId] = useState("");
+  const [resetToken, setResetToken] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const urlUserId = params.get("userid") || "";
+    const urlResetToken = params.get("resetToken") || "";
+
+    if (!urlUserId || !urlResetToken) {
+      toast({
+        description: "Invalid reset link.",
+      });
+      return;
+    }
+
+    setUserId(urlUserId);
+    setResetToken(urlResetToken);
+  }, [toast]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const onSubmit = useCallback(
+    async (values: z.infer<typeof formSchema>) => {
+      setLoading(true);
+      const { password } = values;
+      try {
+        const result = await resetPassword(userId, resetToken, password);
+        if (result.code === "20000" && result.data) {
+          console.log("Reset successful!");
+        } else {
+          toast({
+            description: "Invalid reset token.",
+          });
+        }
+      } catch {
+        toast({
+          description: "Reset failed. Please try again.",
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [toast, userId, resetToken],
+  );
   return (
     <div className=" flex flex-col text-white w-full lg:w-[426px] gap-4">
       <div className="gap-3 flex-col flex">
@@ -78,6 +128,7 @@ const ResetPassword = () => {
               <Button
                 type="submit"
                 className="w-full flex justify-center border border-transparent bg-white text-black-light"
+                disabled={loading}
               >
                 submit
               </Button>

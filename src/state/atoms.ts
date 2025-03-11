dd3c3b634289ea2d0126d015b9bc49168533bc10
AbigailDeng <Abigail.deng@aelf.io>
2025-03-11 11:57:36 +0800

    feat: localstorage

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -1,8 +1,12 @@
 import type { TAtomicAevatar } from "@/assets/schema/atomic-aevatar";
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 export const loadingAtom = atom(false);
 export const atomicAevatarAtom = atom<TAtomicAevatar[]>([]);
 
 // login
-export const accessTokenAtom = atom<string | null>(null);
+export const accessTokenAtom = atomWithStorage<string | null>(
+  "access_token",
+  null,
+);

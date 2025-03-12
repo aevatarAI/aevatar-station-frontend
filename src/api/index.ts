33e54d608d0b3a6661afaf4cd3c381771652963a
potter <potter.sun@aelf.io>
2025-03-12 15:23:24 +0800

    feat: 🎸 add project url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,14 @@
-import { service, spliceUrl } from './axios';
-import { DEFAULT_METHOD, EXPAND_APIS, type EXPAND_REQ_TYPES } from './list';
-import myServer from './server';
-import type { IBaseRequest } from './types';
+import { service, spliceUrl } from "./axios";
+import { DEFAULT_METHOD, EXPAND_APIS, type EXPAND_REQ_TYPES } from "./list";
+import myServer from "./server";
+import type { IBaseRequest } from "./types";
 
-function baseRequest({ url, method = DEFAULT_METHOD, query = '', ...c }: IBaseRequest) {
+function baseRequest({
+  url,
+  method = DEFAULT_METHOD,
+  query = "",
+  ...c
+}: IBaseRequest) {
   return service({
     ...c,
     url: spliceUrl(url, query),

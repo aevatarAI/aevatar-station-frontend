d3c77581351021d5428063e8d8bd2a6730a6f864
Roy Cheong <cjhroy@Roys-MacBook-Pro.local>
2025-03-27 16:29:58 +0800

    fixed access token logic

--- src/hooks/useAccessToken.ts
+++ src/hooks/useAccessToken.ts
@@ -2,7 +2,7 @@ import { accessTokenAtom } from "@/state/atoms";
 import { useAtom } from "jotai";
 
 export const useAccessTokenAtom = () => {
-  const [accessToken] = useAtom(accessTokenAtom)
+  const [accessToken] = useAtom(accessTokenAtom) || localStorage.getItem("access_token");
   const validAccessToken = typeof accessToken === 'string' ? accessToken : '';
   return validAccessToken
 }

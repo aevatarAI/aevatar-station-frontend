d5e6f67d1314f5ec75f241f39e6e84bdbf232616
potter <potter.sun@aelf.io>
2025-03-28 11:30:44 +0800

    feat: 🎸 add refresh token

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -10,7 +10,13 @@ export const accessTokenAtom = atomWithStorage<string | null>(
   "access_token",
   null,
   undefined,
-  { getOnInit: true }
+  { getOnInit: true },
+);
+export const refreshTokenAtom = atomWithStorage<string | null>(
+  "refresh_token",
+  null,
+  undefined,
+  { getOnInit: true },
 );
 // register
 export const emailAtom = atom("");

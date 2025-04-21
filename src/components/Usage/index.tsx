a9cb26ca689b2858604c0a1fe47db6da380d1062
Roy Cheong <cjhroy@192.168.1.8>
2025-04-22 04:18:15 +0800

    update fixes for dashboard mobile

--- src/components/Usage/index.tsx
+++ src/components/Usage/index.tsx
@@ -68,6 +68,23 @@ const UNCHANGED_DATA = [
   },
 ];
 
+export const useIsMobile = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return { isMobile: width <= 768 };
+};
+
 export function Usage() {
   const form = useForm();
   const [apiRequests, setAPIRequests] = useState([]);
@@ -79,6 +96,7 @@ export function Usage() {
   const { data, isLoading } = useGetAPIRequests(date);
   const { data: models } = useGetSystemModels();
   const { data: tokens } = useGetLLMTokens();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     if (data) {
@@ -96,59 +114,71 @@ export function Usage() {
 
   return (
     <div className="pb-[25px]">
-      <div className="flex justify-between items-center">
+      <div className="flex max-[400px]:flex-col max-[400px]:items-start justify-between items-center">
         <span className={clsx(textGradient)}>usage</span>
         <DatePickerWithRange date={date} onDateChange={setDate} />
       </div>
-      <div className="py-[15px]" />
-      <span className="text-[14px] text-gray-light font-semibold">
+      <div className="max-[400px]:h-[1px] max-[400px]:my-[30px] max-[400px]:bg-[#303030] min-[401px]:py-[15px]" />
+      <span className="text-[14px] text-gray-light font-semibold max-[400px]:text-white">
         llms model
       </span>
       <div className="py-[10px]" />
-      <div className="flex justify-between items-center">
-        <div className="flex gap-10">
+      <div className="flex justify-between items-center max-[400px]:grid max-[400px]:justify-normal max-[400px]:items-start">
+        <div className="flex max-[400px]:grid max-[400px]:grid-cols-2 max-[400px]:gap-[5px] gap-[10px]">
           <span className="text-gray-light font-semibold text-[15px]">
-            <strong className="underline text-white">?</strong> total cost
+            <strong className="underline text-white">?</strong>{" "}
+            <span>total cost</span>
           </span>
           <span className="text-gray-light font-semibold text-[15px]">
-            <strong className="underline text-white">?</strong> total input
-            tokens
+            <strong className="underline text-white">?</strong>{" "}
+            <span>total input tokens</span>
           </span>
           <span className="text-gray-light font-semibold text-[15px]">
-            <strong className="underline text-white">?</strong> total output
-            tokens
+            <strong className="underline text-white">?</strong>{" "}
+            <span>total output tokens</span>
           </span>
         </div>
-        <Form {...form}>
-          <FormItem aria-labelledby="models" className="w-[120px]">
-            <Select defaultValue="all models">
-              <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-              </FormControl>
-              <SelectContent className="w-[286px] left-0 -top-[4px] p-[8px_8px_20px_10px] cutCorner cutCorner__white">
-                {models?.data?.length > 0 ? (
-                  models?.data?.map((item: string) => (
-                    <SelectItem className="text-[14px]" key={item} value={item}>
-                      {item}
+        <div className="max-[400px]:py-[10px]" />
+        <div className="justify-self-end self-end">
+          <Form {...form}>
+            <FormItem aria-labelledby="models" className="w-[120px]">
+              <Select defaultValue="all models">
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent className="w-[286px] left-0 -top-[4px] p-[8px_8px_20px_10px] cutCorner cutCorner__white">
+                  {models?.data?.length > 0 ? (
+                    models?.data?.map((item: string) => (
+                      <SelectItem
+                        className="text-[14px]"
+                        key={item}
+                        value={item}
+                      >
+                        {item}
+                      </SelectItem>
+                    ))
+                  ) : (
+                    <SelectItem className="text-[14px]" value="all models">
+                      all models
                     </SelectItem>
-                  ))
-                ) : (
-                  <SelectItem className="text-[14px]" value="all models">
-                    all models
-                  </SelectItem>
-                )}
-              </SelectContent>
-            </Select>
-            <FormMessage />
-          </FormItem>
-        </Form>
+                  )}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          </Form>
+        </div>
       </div>
       <div className="py-[10px]" />
       <div className="bg-[#1e1e1f] pt-[9px] pr-[35px] pb-[24px] pl-0">
         <ResponsiveContainer width="100%" height={302}>
-          <BarChart data={UNCHANGED_DATA} barSize={36} margin={{ top: 40 }}>
+          <BarChart
+            data={UNCHANGED_DATA}
+            barSize={isMobile ? 19 : 36}
+            margin={{ top: 40 }}
+          >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis
@@ -186,7 +216,7 @@ export function Usage() {
         </ResponsiveContainer>
       </div>
       <div className="py-[15px]" />
-      <span className="text-[14px] text-gray-light font-semibold">
+      <span className="text-[14px] text-gray-light font-semibold max-[400px]:text-white">
         api request
       </span>
       <div className="py-[10px]" />

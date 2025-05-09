f22fffb0828e1eee734d882cc3e1b833cccd4286
potter <potter.sun@aelf.io>
2025-05-09 14:08:59 +0800

    feat: 🎸 dll pageadd updated

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -10,9 +10,10 @@ import { itemClassName, itemHoverClassName } from "@/constants/cls";
 import { useNavigate } from "@/hooks/navigate";
 import { useLogout } from "@/hooks/useLogout";
 import { USER_PROFILE_ATOM } from "@/state/atoms/profile";
+import { shortenString } from "@/utils/helpers";
 import clsx from "clsx";
 import { useAtom } from "jotai";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function ProfileAvatar() {
   const navigate = useNavigate();
@@ -20,6 +21,9 @@ export default function ProfileAvatar() {
 
   const logout = useLogout();
   const [profile] = useAtom(USER_PROFILE_ATOM);
+  const profileName = useMemo(() => {
+    return profile?.userName || profile?.name || "";
+  }, [profile]);
 
   return (
     <div className="border-4 border-white rounded-[2px] w-[34px] h-[34px] bg-white">
@@ -28,9 +32,13 @@ export default function ProfileAvatar() {
           <img src={profileImg} alt="profile" className="object-cover" />
         </PopoverTrigger>
         <PopoverContent>
-          {(profile?.userName || profile?.name) && (
+          {profileName && (
             <div className={clsx(itemClassName, "font-semibold font-syne")}>
-              <span>{profile?.userName || profile?.name}</span>
+              <span>
+                {profileName?.length >= 30
+                  ? shortenString(profileName, 5, 5)
+                  : profileName}
+              </span>
             </div>
           )}
           <div className={itemClassName}>

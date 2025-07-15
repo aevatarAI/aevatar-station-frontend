4c0fff4d476ad9158cde910c3ccf9bc5a46ef6dd
potter <potter.sun@aelf.io>
2025-07-15 14:24:55 +0800

    style: 💄 fix profile popover and update sdk version

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./styles/index.css";
+import "./styles/com.css";
 import "@aevatar-react-sdk/ui-react/ui-react.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>

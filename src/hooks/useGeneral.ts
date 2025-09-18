5ec47e2f128a32635b8849106b203f06eff91dc2
potter <potter.sun@aelf.io>
2025-09-18 11:37:02 +0800

    feat: 🎸 update orglist

--- src/hooks/useGeneral.ts
+++ src/hooks/useGeneral.ts
@@ -1,21 +1,26 @@
+import type { IOrganizationItem } from "@/api/utils/organization";
 import { useGetOrganizations } from "@/hooks/useGetOrganizations";
 import { useUpdateOrganisationName } from "@/hooks/useUpdateOrganisationName";
-import type { IOrganizationItem } from "@/api/utils/organization";
-import { CURRENT_ORGANIZATION_ATOM } from "@/state/atoms/organisation";
+import {
+  CURRENT_ORGANIZATION_ATOM,
+  ORGANIZATIONS_LIST_ATOM,
+} from "@/state/atoms/organisation";
 import { useAtom } from "jotai";
 
 export const useGeneral = () => {
-    const [orgId] = useAtom(CURRENT_ORGANIZATION_ATOM);
-    const { data, refetch } = useGetOrganizations();
-    const { mutateAsync } = useUpdateOrganisationName();
-    const handleUpdateName = async (displayName: string) => {
-      await mutateAsync(displayName);
-      refetch();
-    };
-    const currentOrg = data?.data?.items.find(
-      (item: IOrganizationItem) => item.id === orgId
-    );
-  
-    return { handleUpdateName, currentOrg };
-  }
-  
+  const [orgId] = useAtom(CURRENT_ORGANIZATION_ATOM);
+  const [, setOrganizationsList] = useAtom(ORGANIZATIONS_LIST_ATOM);
+  const { data, refetch } = useGetOrganizations();
+  const { mutateAsync } = useUpdateOrganisationName();
+  const handleUpdateName = async (displayName: string) => {
+    await mutateAsync(displayName);
+    const result = await refetch();
+    const list = result.data?.data?.items;
+    list && setOrganizationsList(list);
+  };
+  const currentOrg = data?.data?.items.find(
+    (item: IOrganizationItem) => item.id === orgId,
+  );
+
+  return { handleUpdateName, currentOrg };
+};
